e9b8274d79a0f5086dde6e6661b9712a7703d5fa
Lukasz Jezior <jezior.lukasz89@gmail.com>
2025-07-26 18:13:11 +0200

    First Commit

---
+++ src/test/java/pl/jeziorl/restapp/RestappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jeziorl.restapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
